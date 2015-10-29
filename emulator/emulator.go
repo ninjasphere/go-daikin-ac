2953059f0141c30827a77582a1975c9466e5625e
☃ Elliot Shepherd <elliot@ninjablocks.com>
2015-10-29 15:54:59 +1100

    Small emulator fixes

--- emulator/emulator.go
+++ emulator/emulator.go
@@ -24,14 +24,14 @@ func StartWirelessAC(port int) {
 }
 
 var maxDatagramSize = 8192
-var srvAddr = "224.0.0.1:30050"
+var srvAddr = ":30050"
 var srvAddr2 = "224.0.0.1:30000"
 
 func (d *emulatedWirelessAC) start(port int) {
 
 	go serveMulticastUDP(srvAddr, d.handleUDP)
 
-	go serveMulticastUDP(srvAddr2, d.handleUDP2)
+	//go serveMulticastUDP(srvAddr2, d.handleUDP2)
 
 	/*host, _ := os.Hostname()
 	info := []string{"api=ninja"}
@@ -73,6 +73,8 @@ func (d *emulatedWirelessAC) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	log.Printf("%s", hah)
 	spew.Dump("incoming request", r.URL)
 
+	spew.Dump("request", r)
+
 	if r.URL.String() == "/aircon/get_control_info" {
 		d.getControlInfo(w, r)
 	} else if r.URL.String() == "/aircon/set_control_info" {
@@ -86,14 +88,19 @@ func (d *emulatedWirelessAC) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func (d *emulatedWirelessAC) handleUDP(src *net.UDPAddr, n int, b []byte) {
+func (d *emulatedWirelessAC) handleUDP(conn *net.UDPConn, src *net.UDPAddr, n int, b []byte) {
 	log.Println(n, "udp bytes read from", src)
+
 	log.Println(hex.Dump(b[:n]))
 
-	c, err := net.DialUDP("udp", nil, src)
-	spew.Dump("Dialed back to app", err)
-	i, err := c.Write([]byte("ret=OK,type=aircon,reg=th,dst=1,ver=2_2_5,pow=0,err=0,location=0,name=Ninja%20Blocks%20Wuz%20Here,icon=1,method=polling,port=30050,id=000005zr,pw=pyrbtetn,lpw_flag=0,adp_kind=2,pv=0,cpv=0,led=0,en_setzone=1,mac=FCC2DE460A5C,adp_mode=run\n"))
+	/*addr, err := net.ResolveUDPAddr("udp", "192.168.12.254:30000")
+	if err != nil {
+		log.Fatal(err)
+	}*/
+
+	i, err := conn.WriteToUDP([]byte("ret=OK,type=aircon,reg=th,dst=1,ver=2_2_5,pow=0,err=0,location=0,name=Ninja%20Blocks%20Wuz%20Here,icon=1,method=polling,port=30050,id=000005zr,pw=pyrbtetn,lpw_flag=0,adp_kind=2,pv=0,cpv=0,led=0,en_setzone=1,mac=FCC2DE460A5C,adp_mode=run\n"), src)
 	spew.Dump("Wrote message", i, err)
+
 }
 
 func (d *emulatedWirelessAC) handleUDP2(src *net.UDPAddr, n int, b []byte) {
@@ -101,12 +108,14 @@ func (d *emulatedWirelessAC) handleUDP2(src *net.UDPAddr, n int, b []byte) {
 	log.Println(hex.Dump(b[:n]))
 }
 
-func serveMulticastUDP(a string, h func(*net.UDPAddr, int, []byte)) {
+func serveMulticastUDP(a string, h func(*net.UDPConn, *net.UDPAddr, int, []byte)) {
 	addr, err := net.ResolveUDPAddr("udp", a)
 	if err != nil {
 		log.Fatal(err)
 	}
-	l, err := net.ListenMulticastUDP("udp", nil, addr)
+
+	//en0, _ := net.InterfaceByName("lo0")
+	l, err := net.ListenUDP("udp", addr)
 	if err != nil {
 		panic(err)
 	}
@@ -118,7 +127,7 @@ func serveMulticastUDP(a string, h func(*net.UDPAddr, int, []byte)) {
 		if err != nil {
 			log.Fatal("ReadFromUDP failed:", err)
 		}
-		h(src, n, b)
+		h(l, src, n, b)
 	}
 }
 
